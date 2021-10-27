9053a81e464310c480cc8c011b0dd785ab1ec43b
abutoku <ystkz2021@gmail.com>
2021-10-27 23:32:06 +0900

    update files

--- js/main.js
+++ js/main.js
@@ -72,37 +72,91 @@ var goTimer = function () {
   }
 
 }
-
-var addZero = function (value) {
+//一桁だったら頭に0を足す関数
+let addZero = function (value) {
   if (value < 10) {
     value = '0' + value;
   }
   return value;
 }
-
 ///////////タイマー（ここまで）///////////
 
-//todoリストを作ろう
+///////////////todoリストを作ろう//////////////////
 
-const todos = [];
+const todos = [];//todo用の配列
 
 // データの追加
 $('#add').on('click', function () {
-  todos.push() = $('#todo').val();
-  $('#')
-  // localStorage.setItem('memo', todo);
-  $('#list').append('<p>' + todo + '<p>');
+  $('#list').empty(); //再表示のために一回消す
+
+  const text = $('#todo').val(); //入力されたものを変数に入れる
+  //入力欄を空欄にする
+  
+  $("#todo").val("");
+  $("#todo").focus();
+  //textを配列に入れる
+  todos.push('<div class="txt"><input type="checkbox" class="check">' + text + '<button class ="close fas fa-trash-alt"></button></div>');
+  //console.log(todos); //配列をコンソールに出す
+
+  const jsonData = JSON.stringify(todos); //JSON 形式に変換
+  console.log(jsonData); //JSONをコンソールに出す
+  localStorage.setItem("memo_todo", jsonData);//LocalStorage に情報を保存．
+
+   $('#list').html(todos);//htmlに変換 
 });
+
+
+//ボタンで消す
+$(document).on('click', '.close', function () {
+  const num = $(this).parent().index();
+  console.log(num);
+  todos.splice(num, 1);
+
+  const jsonData = JSON.stringify(todos); //JSON 形式に変換
+  console.log(jsonData); //JSONをコンソールに出す
+  localStorage.setItem("memo_todo", jsonData);
+
+  $('#list').empty();//一旦消して
+  $('#list').html(todos);//再表示
+});
+
 
 // データの削除
 $("#clear").on("click", function () {
-  // localStorage.removeItem("memo");
-  $("input").val("");
+  localStorage.removeItem("memo_todo");
+  $("#todo").val("");
+  $('#list').empty();
 });
 
 // データの取得
-if (localStorage.getItem("memo")) {
-  const text = localStorage.getItem("memo");
-  $("#text_area").val(text);
+if (localStorage.getItem('memo_todo')) {
+  const jsonData = localStorage.getItem("memo_todo");
+  const data = JSON.parse(jsonData);
+  console.log(data);
+  $('#list').html(data);
+  
+  for (let i = 0; i < data.length; i++){ //データを配列に格納
+    todos.push(data[i]);
+  }
+  
 }
 
+$(document).on('click','.check',function () {
+  console.log(this);
+  console.log($(this).is(":checked"));
+  if ($(this).is(":checked")) {
+    $(this).parent().css("text-decoration", "line-through");
+  }
+  else {
+    $(this).parent().css("text-decoration", "none");
+  }
+
+});
+
+
+
+////////////////////memoを作ろう///////////////////////
+
+$("#add_memo").on('click', function(){
+  $('#memo').append('<textarea></textarea>');
+})
